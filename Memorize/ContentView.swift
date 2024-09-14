6bb028ebb4948ea54bc36afe81ae3af0fb99e4e1
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2024-09-14 12:04:57 -0300

    Initial app project template from Xcode

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Manuel Santos Souza on 14/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Manuel Santos Souza on 14/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
