4e5ac0d338875dcd27006e6e280850fe86842483
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2024-09-15 14:45:37 -0300

    Refactoring the cards view.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,11 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            ScrollView {
+                cards
+            }
+            Spacer()
+            cardsCountAdjusters
+        }
+        .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
         }
         .foregroundColor(.orange)
         .padding()
@@ -22,18 +33,19 @@ struct ContentView: View {
 
 struct CardView: View {
     @State private var isFaceUp = false
+    let content: String
     
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
             
-            if isFaceUp {
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2.0)
-                Text("👻").font(.largeTitle)
-            } else {
-                base.fill()
-            }
+                Text(content).font(.largeTitle)
+            }.opacity(isFaceUp ? 1 : 0)
+            
+            base.fill().opacity(isFaceUp ? 0 : 1)
         }.onTapGesture {
             isFaceUp.toggle()
         }
